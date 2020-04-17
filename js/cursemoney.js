de32a8f60e77b56b567205b276e28287d156e857
anton <soldatoff27@gmail.com>
2020-04-17 16:18:14 +0300

    Вывел курс валют перед навигацией в хедере, изменил код js,

--- js/cursemoney.js
+++ js/cursemoney.js
@@ -1,25 +1,27 @@
 //fc3b3db0fb218a7820f0fe94b9403c98
 $(document).ready(function(){
     // $('.money').click(function(){
-        $.get(
-            'http://data.fixer.io/api/latest', 
-            {'access_key': 'fc3b3db0fb218a7820f0fe94b9403c98'}, 
-            function(response){
+        const apiFixer = 'http://data.fixer.io/api/latest';
+
+        const keyFixer = {'access_key': 'fc3b3db0fb218a7820f0fe94b9403c98'};
+
+        let moneyBlock = $('.money_block');
+
+        $.get(apiFixer, keyFixer, function(response){
                 // $('.money_block').text(response.rates);
-                console.log(response.rates.RUB);
-                console.log(response.rates.EUR);
-                console.log(response.rates.USD);
                 // $('.money_block').html(
                 //     `<p>RUB/EUR ${response.rates.RUB}</p>
                 //     <p>EUR ${response.rates.EUR}</p>
                 //     <p>USD/EUR ${response.rates.USD}</p>`);
                     
-                    var dol = response.rates.USD;
+                    let dol = response.rates.USD;
                         euro = response.rates.EUR;
-                        rubEur = (response.rates.RUB * euro).toFixed(3);
-                        rubDol = (rubEur/response.rates.USD).toFixed(3);
+                        rub = response.rates.RUB;
+                        
+                        rubEur = (rub * euro).toFixed(3);
+                        rubDol = (rubEur/dol).toFixed(3);
 
-                    $('.money_block').html(
+                    moneyBlock.html(
                         `<tr>
                             <td>
                                 <b>&#8364</b>
@@ -33,8 +35,6 @@ $(document).ready(function(){
                                 <b>&#8381</b>
                             </td>
                         </tr>
-
-                        
                     `);
                 // $('.money_block').text('Rub ' + response.rates.EUR);
                 // $('.money_block').text('Rub ' + response.rates.USD);
